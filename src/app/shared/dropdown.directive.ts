f8f6df208ed3cf01d0eb58d1a1aecffa47d57017
André Antunes <andre@MacBook-Air-de-Andre.local>
2020-11-26 21:20:02 -0300

    created a dropdown directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,26 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from "@angular/core";
+
+@Directive({
+  selector: "[appDropdown]",
+})
+export class DropDownDirective {
+  @HostBinding("class.open")
+  public open: boolean = false;
+
+  constructor(private elRef: ElementRef) {}
+
+  // @HostListener("click")
+  // public show() {
+  //   this.open = !this.open;
+  // }
+
+  @HostListener("document:click", ["$event"])
+  public clickOut(event: Event): void {
+    this.open = this.elRef.nativeElement.contains(event.target) as boolean;
+  }
+}
